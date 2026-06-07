6bf8cd540b3eadd691e8bf2b09d5dfcb9ba71a5e
Roland Marchand <roland.marchand@protonmail.com>
2026-06-07 15:35:16 -0400

    Added tree-walking to ast to gather node types, worked on analysis

--- ast.go
+++ ast.go
@@ -1,5 +1,9 @@
 package main
 
+type Node interface {
+	node()
+}
+
 type Ast struct {
 	funcs []Func
 	vars  []Var
@@ -45,18 +49,15 @@ func (t Type) String() string {
 }
 
 type Stmt interface {
+	Node
 	stmtNode()
 }
 
 type StmtVar Var
 
-func (s StmtVar) stmtNode() {}
-
 type StmtExpr struct {
 	expr Expr
 }
-
-func (s StmtExpr) stmtNode() {}
 
 type StmtIf struct {
 	test     Expr
@@ -64,8 +65,6 @@ type StmtIf struct {
 	elifs    []StmtElif
 	elseBody []Stmt
 }
-
-func (s StmtIf) stmtNode() {}
 
 type StmtElif struct {
 	test Expr
@@ -77,19 +76,14 @@ type StmtWhile struct {
 	body []Stmt
 }
 
-func (s StmtWhile) stmtNode() {}
-
 type StmtReturn struct {
 	value Expr
 }
 
-func (s StmtReturn) stmtNode() {}
-
 type StmtBreak struct{}
 
-func (s StmtBreak) stmtNode() {}
-
 type Expr interface {
+	Node
 	exprNode()
 }
 
@@ -99,14 +93,10 @@ type ExprBinary struct {
 	op    Op
 }
 
-func (e ExprBinary) exprNode() {}
-
 type ExprUnary struct {
 	value Expr
 	op    Op
 }
-
-func (e ExprUnary) exprNode() {}
 
 type Op int
 
@@ -133,39 +123,148 @@ type ExprCall struct {
 	args   []Expr
 }
 
-func (e ExprCall) exprNode() {}
-
 type ExprField struct {
 	object Expr
 	field  string
 }
 
-func (e ExprField) exprNode() {}
-
 type ExprNumber struct {
 	value int64
 }
-
-func (e ExprNumber) exprNode() {}
 
 type ExprString struct {
 	value string
 }
 
-func (e ExprString) exprNode() {}
-
 type ExprSymbol struct {
 	value string
 }
-
-func (e ExprSymbol) exprNode() {}
 
 type ExprObject struct {
 	fields []Var
 }
 
-func (e ExprObject) exprNode() {}
-
 type ExprNil struct{}
 
+func (a Ast) node(){}
+func (f Func) node(){}
+func (p Params) node(){}
+func (v Var) node(){}
+func (t Type) node(){}
+func (s StmtVar) node() {}
+func (s StmtExpr) node() {}
+func (s StmtIf) node() {}
+func (s StmtElif) node() {}
+func (s StmtWhile) node() {}
+func (s StmtReturn) node() {}
+func (s StmtBreak) node() {}
+func (e ExprBinary) node() {}
+func (e ExprUnary) node() {}
+func (e ExprCall) node() {}
+func (e ExprField) node() {}
+func (e ExprNumber) node() {}
+func (e ExprString) node() {}
+func (e ExprSymbol) node() {}
+func (e ExprObject) node() {}
+func (e ExprNil) node() {}
+
+func (s StmtVar) stmtNode() {}
+func (s StmtExpr) stmtNode() {}
+func (s StmtIf) stmtNode() {}
+func (s StmtElif) stmtNode() {}
+func (s StmtWhile) stmtNode() {}
+func (s StmtReturn) stmtNode() {}
+func (s StmtBreak) stmtNode() {}
+func (e ExprBinary) exprNode() {}
+func (e ExprUnary) exprNode() {}
+func (e ExprCall) exprNode() {}
+func (e ExprField) exprNode() {}
+func (e ExprNumber) exprNode() {}
+func (e ExprString) exprNode() {}
+func (e ExprSymbol) exprNode() {}
+func (e ExprObject) exprNode() {}
 func (e ExprNil) exprNode() {}
+
+func findNodes[T Node](root Node) []T {
+	if root == nil {
+		return nil
+	}
+
+	var result []T
+
+	if t, ok := root.(T); ok {
+		result = append(result, t)
+	}
+
+	switch e := root.(type) {
+	// Other
+	case Ast:
+		for _, n := range e.funcs {
+			result = append(result, findNodes[T](n)...)
+		}
+		for _, n := range e.vars {
+			result = append(result, findNodes[T](n)...)
+		}
+	case Func:
+		result = append(result, findNodes[T](e.params)...)
+		result = append(result, findNodes[T](e.ret)...)
+		for _, n := range e.body {
+			result = append(result, findNodes[T](n)...)
+		}
+	case Params:
+		for _, n := range e.vars {
+			result = append(result, findNodes[T](n)...)
+		}
+	case Var:
+		result = append(result, findNodes[T](e.kind)...)
+		result = append(result, findNodes[T](e.assigned)...)
+	// Statements
+	case StmtVar:
+		result = append(result, findNodes[T](e.kind)...)
+		result = append(result, findNodes[T](e.assigned)...)
+	case StmtExpr:
+		result = append(result, findNodes[T](e.expr)...)
+	case StmtIf:
+		result = append(result, findNodes[T](e.test)...)
+		for _, n := range e.body {
+			result = append(result, findNodes[T](n)...)
+		}
+		for _, n := range e.elifs {
+			result = append(result, findNodes[T](n)...)
+		}
+		for _, n := range e.elseBody {
+			result = append(result, findNodes[T](n)...)
+		}
+	case StmtElif:
+		result = append(result, findNodes[T](e.test)...)
+		for _, b := range e.body {
+			result = append(result, findNodes[T](b)...)
+		}
+	case StmtWhile:
+		result = append(result, findNodes[T](e.test)...)
+		for _, n := range(e.body) {
+			result = append(result, findNodes[T](n)...)
+		}
+	case StmtReturn:
+		result = append(result, findNodes[T](e.value)...)
+	// Expressions
+	case ExprBinary:
+		result = append(result, findNodes[T](e.left)...)
+		result = append(result, findNodes[T](e.right)...)
+	case ExprUnary:
+		result = append(result, findNodes[T](e.value)...)
+	case ExprCall:
+		result = append(result, findNodes[T](e.callee)...)
+		for _, arg := range e.args {
+			result = append(result, findNodes[T](arg)...)
+		}
+	case ExprField:
+		result = append(result, findNodes[T](e.object)...)
+	case ExprObject:
+		for _, field := range e.fields {
+			result = append(result, findNodes[T](field)...)
+		}
+	}
+
+	return result
+}
